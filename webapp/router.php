c0310258327ac42fecdff5b910dd87d5541c7837
João Antunes <2190732@my.ipleiria.pt>
2020-06-14 12:50:59 +0100

    Initialization of webapp and wlstudio

---
+++ webapp/router.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smendes
+ * Date: 02-05-2016
+ * Time: 11:18
+ */
+use ArmoredCore\Facades\Router;
+
+/****************************************************************************
+ *  URLEncoder/HTTPRouter Routing Rules
+ *  Use convention: controllerName@methodActionName
+ ****************************************************************************/
+
+Router::get('/',			'HomeController/index');
+Router::get('home/',		'HomeController/index');
+Router::get('home/index',	'HomeController/index');
+Router::get('home/start',	'HomeController/start');
+Router::get('home/login',	'HomeController/login');
+Router::get('home/about', 'HomeController/about');
+
+Router::get('game/', 'GameController/index');
+Router::get('game/index', 'GameController/index');
+Router::get('game/register', 'GameController/register');
+Router::post('game/createAccount', 'GameController/createAccount');
+Router::get('game/login', 'GameController/login');
+Router::post('game/authenticate', 'GameController/authenticate');
+Router::post('game/logout', 'GameController/logout');
+Router::post('game/game', 'GameController/game');
+
+Router::post('gameLogic/lancarDados', 'GameLogicController/lancarDados');
+
+
+
+
+
+
+
+
+/************** End of URLEncoder Routing Rules ************************************/
